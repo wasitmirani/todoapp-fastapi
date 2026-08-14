01a02a8a49dc87df73fa4e8253186f5971755af1
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2026-08-14 12:25:40 -0700

    Implement task creation logic in controller and update router to handle database session

--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -1,5 +1,17 @@
+from dtos.task_dtos import TaskCreateDto
+from sqlalchemy.orm import Session
+from models.task import Task
+from datetime import datetime
 
-
-def create_task(task: Task | None = None) -> Task:
-    
-    return {"message": "Task created successfully", "task": task}
+def create_task(task: TaskCreateDto,db: Session | None = None) -> TaskCreateDto:
+    data = task.model_dump()
+    new_task = Task(title=data['title'], 
+    description=data['description'], 
+    completed=data['completed'],
+    created_at=datetime.now(),
+    updated_at=datetime.now()
+    )
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return new_task
--- routes/task_router.py
+++ routes/task_router.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
 from controllers.task_controller import create_task
 from dtos.task_dtos import TaskCreateDto
+from utils.db import get_db
+from sqlalchemy.orm import Session
 
 task_router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 
 @task_router.post('/store')
-def store_task(task: TaskCreateDto):
-    return create_task(task)
+def store_task(task: TaskCreateDto, db: Session = Depends(get_db)):
+    return {"message": "Task created successfully", "task": create_task(task, db)}

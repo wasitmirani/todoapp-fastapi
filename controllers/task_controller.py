3a8aadb2df45c1518a95571a2afebd312f8d2253
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2026-08-19 11:00:16 -0700

    Add task retrieval and update functionality in controller, and enhance router with new endpoints for fetching and updating tasks

--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -25,3 +25,25 @@ def get_tasks(db:Session | None = None):
     tasks= db.query(Task).order_by(Task.created_at.desc()).all()
     return {"tasks": tasks,"message": "Tasks fetched successfully","status": "success"}
 
+
+def get_task(id:int, db:Session | None = None):
+    if db is None:
+        raise HTTPException(status_code=400, detail="Database session not found")
+    task  =db.query(Task).where(Task.id == id).first()
+    if task is None:
+         raise HTTPException(status_code=404, detail="Task not found")
+    return {"task": task,"message": "Task fetched successfully","status": "success"}
+
+def update_task(id:int,task: TaskCreateDto, db:Session | None = None):
+    if db is None:
+        raise HTTPException(status_code=400, detail="Database session not found")
+    task_data = db.query(Task).where(Task.id == id).first()
+    if task_data is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    task_data.title = task.title
+    task_data.description = task.description
+    task_data.completed = task.completed
+    task_data.updated_at = datetime.now()
+    db.commit()
+    db.refresh(task_data)
+    return {"task": task_data,"message": "Task updated successfully","status": "success"}
--- routes/task_router.py
+++ routes/task_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter,Depends
-from controllers.task_controller import create_task,get_tasks
+from controllers.task_controller import create_task,get_tasks,get_task,update_task
 from dtos.task_dtos import TaskCreateDto
 from utils.db import get_db
 from sqlalchemy.orm import Session
@@ -14,3 +14,12 @@ def store_task(task: TaskCreateDto, db: Session = Depends(get_db)):
 @task_router.get('/items')
 def get_list(db: Session = Depends(get_db)):
     return get_tasks(db)
+
+@task_router.get('/{id}')
+def show_task(id:int, db: Session = Depends(get_db)):
+    return get_task(id, db)
+
+
+@task_router.put('/{id}')
+def update(id:int ,task: TaskCreateDto, db: Session = Depends(get_db)):
+    return update_task(id, task, db)
